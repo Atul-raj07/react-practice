e5f01c109fe1fdca7fb8b30a0e56046fb2ab64b8
Atul-raj07 <atulrajgupta12j@gmail.com>
2024-10-05 19:39:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import Header from './components/Header';
+const styles = {
+  fontSize:"80px",
+  color:"yellow",
+}
+
+const App = () => {
+  return (
+    <div>
+    <div style={{fontSize:'40px',backgroundColor:"black",color:'pink'}}>
+      App</div>
+      <h2 style={styles}>mumijckmsdk</h2>
+      <h1>njfijkjfjfdkjjiofd</h1>
+      <h1 className='lolo'>hlw darling</h1>
+      <Header />
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+import "../index.css";
+import css from "../components/Header.module.css"
+
+const Header = () => {
+    
+    
+  return (
+    <div>
+        Header
+        <h1 className='lolo'>im from header</h1>
+        <h2 className={css.something} >lolkookl222222</h2>
+        </div>
+  )
+}
+
+export default Header
